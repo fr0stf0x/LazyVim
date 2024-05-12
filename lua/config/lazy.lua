49e785d021c727947bd66c00c84fc975176e0c65
fr0stf0x <tang.xuanthao1996@gmail.com>
2024-05-12 22:25:43 +0700

    configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
+
     -- { import = "lazyvim.plugins.extras.ui.edgy" },
     -- { import = "lazyvim.plugins.extras.editor.navic" },
     -- { import = "lazyvim.plugins.extras.editor.aerial" },
