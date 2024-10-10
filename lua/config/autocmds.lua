2599da2b97d61c2c55d8c4f92ec36d8ed9f7ad9b
fr0stf0x <tang.xuanthao1996@gmail.com>
2024-10-10 14:02:37 +0700

    update nvchad with lazyvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,4 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.cmd("Nvdash")
+if not vim.g.vscode then
+  vim.cmd("Nvdash")
+end
